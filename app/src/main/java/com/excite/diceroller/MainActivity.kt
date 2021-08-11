c69f24fdc3a057b058e4206786f7b896bc1edd26
excitexcite <excitenew@gmail.com>
2021-08-11 16:09:31 +0300

    Add ability to rand dice value by clicking the button

--- app/src/main/java/com/excite/diceroller/MainActivity.kt
+++ app/src/main/java/com/excite/diceroller/MainActivity.kt
@@ -3,7 +3,9 @@ package com.excite.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,8 +14,14 @@ class MainActivity : AppCompatActivity() {
         val rollButton = findViewById<Button>(R.id.roll_button)
         rollButton.text = "Let`s roll"
         rollButton.setOnClickListener{
-            Toast.makeText(this, "Button clicked", Toast.LENGTH_SHORT)
-                .show()
+//            Toast.makeText(this, "Button clicked", Toast.LENGTH_SHORT).show()
+            rollDice()
         }
+    }
+
+    private fun rollDice() {
+        val resultText = findViewById<TextView>(R.id.result_text)
+        val randInt = Random.nextInt(6) + 1
+        resultText.text = randInt.toString()
     }
 }
