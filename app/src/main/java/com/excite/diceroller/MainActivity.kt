d8bb36036c3e86fb08d63c85466ad8ed67b85dd5
excitexcite <excitenew@gmail.com>
2021-08-11 17:56:27 +0300

    Use lateinit class field for ImageView variable instead of local ImageView variable in method rollDice()

--- app/src/main/java/com/excite/diceroller/MainActivity.kt
+++ app/src/main/java/com/excite/diceroller/MainActivity.kt
@@ -9,6 +9,9 @@ import android.widget.Toast
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage : ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -18,6 +21,8 @@ class MainActivity : AppCompatActivity() {
 //            Toast.makeText(this, "Button clicked", Toast.LENGTH_SHORT).show()
             rollDice()
         }
+
+        diceImage = findViewById<ImageView>(R.id.dice_image)
     }
 
     private fun rollDice() {
@@ -30,7 +35,6 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
-        val diceImage = findViewById<ImageView>(R.id.dice_image)
         diceImage.setImageResource(drawableResource)
     }
 }
